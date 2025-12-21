33a9da03cd3a376b99ef6d5337eb64a96fb2b270
tjmax79 <ogidantunji@gmail.com>
2025-12-21 20:40:43 +0100

    Add Book constructor and addBookToLibrary function

---
+++ index.js
@@ -0,0 +1,20 @@
+const myLibrary  =  [];
+
+function Book (title,author,pages,read) {
+      this.id = crypto.randomUUID();
+      this.title = title;
+      this.author = author;
+      this.pages = pages;
+      this.read = read;
+}
+
+function addBookToLibrary (title,author,pages,read){
+    const newBook = new Book (title,author,pages,read)
+    myLibrary.push(newBook)
+}
+
+addBookToLibrary("Osanle", 'Nike', 295, true)
+addBookToLibrary('Be you', "Tunde", 50, false)
+addBookToLibrary('Absolute', 'George', 300,true )
+console.log(myLibrary);
+
