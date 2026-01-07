f17db564bf364852f1ca17fae8c12c6706c8a313
tjmax79 <ogidantunji@gmail.com>
2026-01-07 12:38:49 +0100

    Update index files with recent library project features

--- index.js
+++ index.js
@@ -18,10 +18,48 @@ addBookToLibrary('Be you', "Tunde", 50, false)
 addBookToLibrary('Absolute', 'George', 300,true )
 console.log(myLibrary);
 
-function displayBook () {
-    for(let key in myLibrary){
-        return (key);
-    }
-}
 
-console.log(key);
+function displayBookOnPage () {
+    const container = document.querySelector("#container");
+
+    // while(container.firstChild){
+    //     container.removeChild(container.firstChild)
+    // }
+
+
+myLibrary.forEach((book) =>{ 
+    // Create the main card container
+    const bookCard = document.createElement("div");
+    bookCard.style.cssText = "width:400px; border:2px solid black;border-radius:5px;padding!5px,margin:10px"
+
+    
+// create and style the title element
+const titleElement = document.createElement("h3");
+titleElement.textContent = book.title;
+titleElement.style.textAlign ="center"
+
+//create and style the Author element
+const authorElement = document.createElement("p");
+authorElement.textContent = `Author : ${book.author}`
+
+// create and style the pages element
+
+const pagesElement = document.createElement("p");
+pagesElement.textContent = `Pages: ${book.pages}`;
+
+//create read status
+const readElement = document.createElement ('p');
+readElement.textContent = `Status: ${book.read} "Read" : "Not Read"`
+
+// append all pieces to the car
+
+ bookCard.appendChild(titleElement);
+        bookCard.appendChild(authorElement);
+        bookCard.appendChild(pagesElement);
+        bookCard.appendChild(readElement);
+
+container.appendChild(bookCard)
+ 
+})
+}
+displayBookOnPage()
